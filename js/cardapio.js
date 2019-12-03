a26c143d777fb303065c1e0cd6bc9fdeb3480840
Keila <keiladaianefernandes24@gmail.com>
2019-12-03 19:03:31 -0300

    Editar cardápio terminado

--- js/cardapio.js
+++ js/cardapio.js
@@ -6,21 +6,16 @@ const APICARDAPIO = axios.create({
     }
 });
 
-let btnCadastroCardapio = document.getElementById('btn-cadastro-cardapio')
-let btnEditarCardapio = document.getElementById('btn-editar-cardapio')
-
-let todayMenu = document.getElementById('today-menu')
-
-
-let data = document.getElementsByClassName('data')
-
 dataAtual = new Date
 
+// ****************************************************************************************************************************
+// **************************************************** Cardápio do dia *******************************************************
+// ****************************************************************************************************************************
 
 APICARDAPIO.get(`/cardapio/${dataAtual.getDate()}`)
     .then(function (response) {
         // Sucesso
-
+        let todayMenu = document.getElementById('today-menu')
         if (response.data == "") {
             alertify.warning('Cardápio do dia ainda não cadastrado!')
 
@@ -47,15 +42,15 @@ APICARDAPIO.get(`/cardapio/${dataAtual.getDate()}`)
             <p id="dishSeven" class="item">- ${response.data.setimoPrato}</p>
         `
         }
-        console.log(response)
-
     })
     .catch(function (error) {
         // Erro
         alertify.warning('Cardápio do dia ainda não cadastrado ou indisponível!');
     });
 
-
+// ****************************************************************************************************************************
+// ************************************************* Registrar um cardápio ****************************************************
+// ****************************************************************************************************************************
 function submitRegister() {
     let date = new Date(`${document.getElementById("inputDate").value} 01:00:00`);
     let JSONCardapio = {
@@ -87,9 +82,125 @@ function submitRegister() {
         .catch(function (error) {
             alertify.error('Erro ao cadastrar cardápio!');
         });
-
 }
 
+// ****************************************************************************************************************************
+// **************************************************** Editar um cardápio ****************************************************
+// ****************************************************************************************************************************
+
+// procura o cardápio a ser editado
+function putSearchFields() {
+    let diaProcurado = document.getElementById('inputEdit').value
+    APICARDAPIO.get(`/cardapio/${diaProcurado}`)
+        .then(function (response) {
+            // Sucesso
+            if (response.data == "") {
+                alertify.error(`Cardápio do dia ${diaProcurado}/${dataAtual.getMonth()+1} ainda não cadastrado ou indisponível!`);
+            } else {
+                let id = response.data.id
+                let dia = response.data.dia
+                let mes = response.data.mes
+                let ano = response.data.ano
+
+                document.getElementById('sub-edit').innerHTML = `
+                <p class="title">Edite os valores abaixo</p>
+
+                <label>1º Prato *</label>
+                <input type="text" id="inputDishOneEdit" autocomplete="on">
+
+                <label>2º Prato *</label>
+                <input type="text" id="inputDishTwoEdit" autocomplete="on">
+
+                <label>3º Prato *</label>
+                <input type="text" id="inputDishThreeEdit" autocomplete="on">
+
+                <label>4º Prato *</label>
+                <input type="text" id="inputDishFourEdit" autocomplete="on">
+
+                <label>5º Prato *</label>
+                <input type="text" id="inputDishFiveEdit" autocomplete="on">
+
+                <label>6º Prato *</label>
+                <input type="text" id="inputDishSixEdit" autocomplete="on">
+
+                <label>7º Prato</label>
+                <input type="text" id="inputDishSevenEdit" autocomplete="on">
+
+                <div class="submits">
+                    <input type="button" id="button-submit-edit" value="Reenviar" class="btn-submit" onclick="submitEdit(${id}, ${dia}, ${mes}, ${ano})">
+                    <input type="button" id="button-cancel-edit" value="Cancelar" class="btn-cancel" onclick="clearField()">
+                </div>
+            `
+                document.getElementById('inputDishOneEdit').value = response.data.primeiroPrato
+                document.getElementById('inputDishTwoEdit').value = response.data.segundoPrato
+                document.getElementById('inputDishThreeEdit').value = response.data.terceiroPrato
+                document.getElementById('inputDishFourEdit').value = response.data.quartoPrato
+                document.getElementById('inputDishFiveEdit').value = response.data.quintoPrato
+                document.getElementById('inputDishSixEdit').value = response.data.sextoPrato
+                document.getElementById('inputDishSevenEdit').value = response.data.setimoPrato
+            }
+        }).catch(function (error) {
+            if (diaProcurado == "") {
+                alertify.error(`Campo do cardápio do dia está vazio!`);
+            } else {
+                alertify.error(`Cardápio do dia ${diaProcurado}/${dataAtual.getMonth} ainda não cadastrado ou indisponível!`);
+            }
+        });
+}
+// envai o cardápio editado
+function submitEdit(id,dia, mes, ano) {
+    let JSONCardapioEdit = {
+        "id": `${id}`,
+        "dia": `${dia}`,
+        "mes": `${mes}`,
+        "ano": `${ano}`,
+        "primeiroPrato": `${document.getElementById("inputDishOneEdit").value}`,
+        "segundoPrato": `${document.getElementById("inputDishTwoEdit").value}`,
+        "terceiroPrato": `${document.getElementById("inputDishThreeEdit").value}`,
+        "quartoPrato": `${document.getElementById("inputDishFourEdit").value}`,
+        "quintoPrato": `${document.getElementById("inputDishFiveEdit").value}`,
+        "sextoPrato": `${document.getElementById("inputDishSixEdit").value}`,
+        "setimoPrato": `${document.getElementById("inputDishSevenEdit").value}`,
+    }
+    console.log("Editado: " + JSONCardapioEdit);
+    APICARDAPIO.put('/cardapio', JSONCardapioEdit)
+        .then(function (response) {
+            // Retira os campos
+            clearField()
+            alertify.success('Cardápio editado com sucesso!');
+        })
+        .catch(function (error) {
+            alertify.error('Erro ao editar cardápio!');
+        });
+}
+// reseta os campos e some com os forms
+function clearField() {
+    document.getElementById('inputDishOneEdit').value = ""
+    document.getElementById('inputDishTwoEdit').value = ""
+    document.getElementById('inputDishThreeEdit').value = ""
+    document.getElementById('inputDishFourEdit').value = ""
+    document.getElementById('inputDishFiveEdit').value = ""
+    document.getElementById('inputDishSixEdit').value = ""
+    document.getElementById('inputDishSevenEdit').value = ""
+    document.getElementById('inputEdit').value = "" // limpa o campo de procura
+    document.getElementById('sub-edit').innerHTML = `` // some com os forms
+}
+
+// ****************************************************************************************************************************
+// **************************************************** Deletar um cardápio ***************************************************
+// ****************************************************************************************************************************
+
+
+
+// ****************************************************************************************************************************
+// ************************************************** Procurar um cardápio ****************************************************
+// ****************************************************************************************************************************
+
+
+
+// ****************************************************************************************************************************
+// ******************************************** Troca de telas das funções ****************************************************
+// ****************************************************************************************************************************
 function showRegister() {
     // Abrir conteúdo
     document.getElementById("today-menu").style.display = "none";
@@ -145,6 +256,3 @@ function showSearch() {
     document.getElementById("delete").classList.remove("btn-selected")
     document.getElementById("search").classList.add("btn-selected")
 }
-
-
-// 
